baab77822494f2788db39738a2eff1b5c876eee0
Dhi Aurrahman <dio@rockybars.com>
2022-10-24 06:15:04 +0700

    Fix
    Signed-off-by: Dhi Aurrahman <dio@rockybars.com>

--- tools/github/write_current_source_version.py
+++ tools/github/write_current_source_version.py
@@ -58,5 +58,5 @@ if __name__ == '__main__':
                                 + current_version) as response:
         commit_info = json.loads(response.read())
         source_version_file = pathlib.Path('SOURCE_VERSION')
-        # Write the extracted current version commit hash 'sha' to SOURCE_VERSION.
+        # Write the extracted current version commit hash "sha" to SOURCE_VERSION.
         source_version_file.write_text(commit_info['sha'])
